dcbe472b044b1ea2877c990a65ffa6bac26b7da9
KalangiMounami <mounamisudheer@gmail.com>
2025-12-26 09:46:36 +0530

    Update mqtt_subscriber.c

--- software/codes/mqtt_subscriber.c
+++ software/codes/mqtt_subscriber.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "MQTTClient.h"
 
+#define ADDRESS "tcp://broker.hivemq.com:1883"
+#define CLIENTID "SmartBinSubscriber"
+#define TOPIC "smartbin/data"
+#define TIMEOUT 10000L
+
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
+{
+    printf("Message: %s\n", (char *)message->payload);
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+    return 1;
+}
+
+int main()
+{
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+
+    MQTTClient_create(&client, ADDRESS, CLIENTID,
+                      MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    MQTTClient_setCallbacks(client, NULL, NULL, msgarrvd, NULL);
+
+    MQTTClient_connect(client, &conn_opts);
+    MQTTClient_subscribe(client, TOPIC, 0);
+
+    while (1);
+    MQTTClient_disconnect(client, TIMEOUT);
+    MQTTClient_destroy(&client);
+    return 0;
+}
